01f85f1d44a8b0161817fadb5d8ee0096066a602
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2024-02-22 19:32:30 +0000

    Added route for user, added controller and view for show

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   root "photos#index"
 
-  get "/users/:id" => "users#show", as: :user
-
   devise_for :users
   resources :comments
   resources :follow_requests
   resources :likes
   resources :photos
+
+  # if I wanted "/users/:id", could specify the path in either of the two ways described
+  # 1. get "/users/:id" => "users#show", as: :user
+  # 2. resources :users, only: :show
+
+  get "/:username" => "users#show", as: :user
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
